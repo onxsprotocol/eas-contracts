341cc2bfd5aa9ad7a41937754a607eebf72d3ad5
icaksh <me@icaksh.my.id>
2024-09-05 07:20:28 +0000

    add: glide protocol

---
+++ utils/Constants.ts
@@ -0,0 +1,31 @@
+import { ZeroAddress } from 'ethers';
+
+export enum DeploymentNetwork {
+  Mainnet = 'mainnet',
+  Optimism = 'optimism',
+  Base = 'base',
+  ArbitrumOne = 'arbitrum-one',
+  ArbitrumNova = 'arbitrum-nova',
+  Polygon = 'polygon',
+  Scroll = 'scroll',
+  ZKSync = 'zksync',
+  Celo = 'celo',
+  Linea = 'linea',
+  Sepolia = 'sepolia',
+  OptimismSepolia = 'optimism-sepolia',
+  OptimismGoerli = 'optimism-goerli',
+  BaseSepolia = 'base-sepolia',
+  BaseGoerli = 'base-goerli',
+  ArbitrumGoerli = 'arbitrum-goerli',
+  PolygonAmoy = 'polygon-amoy',
+  ScrollSepolia = 'scroll-sepolia',
+  LineaGoerli = 'linea-goerli',
+  Hardhat = 'hardhat',
+  Glide = 'glide',
+}
+
+export const ZERO_ADDRESS = ZeroAddress;
+export const ZERO_BYTES = '0x';
+export const ZERO_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
+export const NO_EXPIRATION = 0n;
